296422d48eb2bba24d7b6385a0fde9e919f3b79c
speric <eric@prudentiadigital.com>
2011-07-27 14:18:23 -0400

    Posts index CSS (needs to move to stylesheet)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.order("created_at DESC")
+    @posts = Post.order("created_at DESC").includes(:book)
 
     respond_to do |format|
       format.html # index.html.erb
